ac425559f139e52d3e5de16b667b3e692b808d7b
Toby Thomson <me@tobythomson.co.uk>
2023-04-15 20:07:05 +0100

    Modified main.py to use new pressure circuit format for coolant pressure calcs

--- src/studies/10-bar-sizing/main.py
+++ src/studies/10-bar-sizing/main.py
@@ -2,14 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from deepdiff import DeepDiff
 from pprint import pprint
+from CoolProp.CoolProp import PropsSI
 from rafiki.rafiki import Rafiki, RafikiInputParameters, RafikiOutputParameters
 from yeti.yeti import Yeti, YetiInputParameters
+from yeti.pressure_drop_circuit import PressureDropCircuit
 import yeti.yeti
 from injector import TripletInjector
 
-# Using IPA and LOX densities respecitivey
+# TODO: Think about absoluet vs gauge pressure...
 FuelDensity__kg_per_m3 = 786
+FuelViscosity__Pa_s=0.00237
+
 OxidiserDensity__kg_per_m3 = 1141
+OxidiserViscosity__Pa_s=0.000197
 
 InputParameters = RafikiInputParameters(	fuel="Isopropanol",
 											oxidiser="LOX",
@@ -50,10 +55,10 @@ CoolingParameters = YetiInputParameters(	chamberPressure__bar=InputParameters.ch
 # TODO: Review element count
 Injector = TripletInjector(dischargeCoefficient=0.65, elementCount=8, pressureDrop__bar=6)
 
-def calculateInjectorGeometry(mf_dot__kg_per_s, mo_dot__kg_per_s, fuelDensity__kg_per_m3, oxdiserDensity__kg_per_m3, injector):
+def calculateInjectorGeometry(fuelMassFlowRate__kg_per_s, oxidiserMassFlowRate__kg_per_s, fuelDensity__kg_per_m3, oxdiserDensity__kg_per_m3, injector):
 	# Source: http://athena.leedsrocketry.co.uk/doku.php?id=louie_injector_desgin
-	fuelArea__mm2 = (mf_dot__kg_per_s / (injector.dischargeCoefficient * np.sqrt(2 * fuelDensity__kg_per_m3 * injector.pressureDrop__Pa))) * 10**6
-	oxidiserArea__mm2 = (mo_dot__kg_per_s / (injector.dischargeCoefficient * np.sqrt(2 * oxdiserDensity__kg_per_m3 * injector.pressureDrop__Pa))) * 10**6
+	fuelArea__mm2 = (fuelMassFlowRate__kg_per_s / (injector.dischargeCoefficient * np.sqrt(2 * fuelDensity__kg_per_m3 * injector.pressureDrop__Pa))) * 10**6
+	oxidiserArea__mm2 = (oxidiserMassFlowRate__kg_per_s / (injector.dischargeCoefficient * np.sqrt(2 * oxdiserDensity__kg_per_m3 * injector.pressureDrop__Pa))) * 10**6
 
 	injector.fuelOrificeDiameter__mm = np.sqrt((2 * fuelArea__mm2) / (injector.elementCount * np.pi))
 	injector.oxidiserOrificeDiameter__mm = 2 * np.sqrt(oxidiserArea__mm2 / (injector.elementCount * np.pi))
@@ -63,15 +68,15 @@ def displayFineTunedParameters(rafikiOutput):
 	pprint(DeepDiff(rafikiOutput[0].outputParameters, FineTunedRafikiOutputParameters, ignore_order=True, ignore_numeric_type_changes=True, significant_digits=3)["values_changed"])
 
 def calculateBurnTime(outputParameters):
-	# Work out the maximum burn time according to the tank volumes and denisties of the fuel and oxidiser according to AEL's ICD
-	# (we've assumed the LOX tank volume will be no less than that of the NO2 tank)
+	# Work out the maximum burn time according to the tank volumes and denisties of the fuel and oxidiser according to AEL"s ICD
+	# (we"ve assumed the LOX tank volume will be no less than that of the NO2 tank)
 	maximumFuelBurnTime__s = ((3.5 * 0.786) / outputParameters.mf_dot__kg_per_s.value)
 	maximumOxidiserBurnTime__s = ((10 * 1.141) / outputParameters.mo_dot__kg_per_s.value)
 	maximumBurnTime__s = min(maximumFuelBurnTime__s, maximumOxidiserBurnTime__s)
 
 	return maximumBurnTime__s
 
-def calculateCoolantPressureDrop(rafikiOutput, inputParameters, cooling_data):
+def calculateCoolantPressureDrop(coolingParameters, cooling_data):
 	"""
 	Pressure drop assumptions:
 		- Inlet and outlet distribution rings are pipes curved along a half-circular arc of the
@@ -82,13 +87,8 @@ def calculateCoolantPressureDrop(rafikiOutput, inputParameters, cooling_data):
 			pipe.
 	"""
 
-	T = inputParameters.coolantInletTemperature__K
-	p = inputParameters.coolantInletPressure__Pa
-
-	formattedEngineGeometry__m = np.array(rafikiOutput.engineGeometry__mm.value).transpose() / 1000
-
-	xs__m = formattedEngineGeometry__m[0]
-	rs__m = formattedEngineGeometry__m[1]
+	T = coolingParameters.coolantInletTemperature__K
+	p = coolingParameters.coolantInletPressure__Pa
 	
 	cool_delivery_len__m = 1 # length of coolant delivery pipe [m]
 	cool_delivery_d__m = 0.025 # diameter of coolant delivery pipe [m]
@@ -114,168 +114,138 @@ def calculateCoolantPressureDrop(rafikiOutput, inputParameters, cooling_data):
 	channel_width__m = (2/1000)
 
 	# build the pressure drop circuit
-	pressure_circuit = PressureDropCircuit(inputParameters.coolant.laminarRe, inputParameters.coolant.turbulentRe)
+	pressure_circuit = PressureDropCircuit( massFlowRate__kg_per_s=coolingParameters.coolantMassFlowRate__kg_per_s,
+											fluidDensity__kg_per_m3=coolingParameters.coolant.density__kg_per_m3(T, p),
+											fluidViscosity__Pa_s=coolingParameters.coolant.viscosity__Pa_s(T, p)	)
 	# delivery pipe
-	pressure_circuit.add_straight_pipes_circ(r__m=cool_delivery_d__m/2,
-													n_pipes=1,
-													len__m=cool_delivery_len__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-													label="Delivery pipe")
+	pressure_circuit.add_straight_pipes_circ(	r__m=cool_delivery_d__m/2,
+												n_pipes=1,
+												len__m=cool_delivery_len__m,
+												label="Delivery pipe"	)
 	# halo inlet pipe 
-	pressure_circuit.add_straight_pipes_circ(r__m=cool_halo_pipe_d__m/2,
-													n_pipes=1,
-													len__m=cool_halo_inlet_len__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-													label="Halo inlet pipe")
+	pressure_circuit.add_straight_pipes_circ(	r__m=cool_halo_pipe_d__m/2,
+												n_pipes=1,
+												len__m=cool_halo_inlet_len__m,
+												label="Halo inlet pipe"	)
 										
 	# halo distribution ring
-	pressure_circuit.add_circular_pipe_circular(r__m=cool_halo_ring_d__m/2,
-														rp__m=cool_halo_tube_d__m/2,
-														percentage_travelled=0.5,
-														mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-														rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-														mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-														n=1,
-														label="Halo distribution inlet")
+	pressure_circuit.add_circular_pipe_circular(	r__m=cool_halo_ring_d__m/2,
+													rp__m=cool_halo_tube_d__m/2,
+													percentage_travelled=0.5,
+													n_pipes=1,
+													label="Halo distribution inlet"	)
 
 	# halo turn into cooling jacket inlet
-	pressure_circuit.add_90deg_turn_circular(r__m=cool_inlet_pipe_d__m/2,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s/4,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													n=3,
-													label="Halo turn into jacket inlet")
+	pressure_circuit.add_90deg_turn_circular(	r__m=cool_inlet_pipe_d__m/2,
+												n_pipes=4,
+												label="Halo turn into jacket inlet"	)
 
 	# inlet pipe
-	pressure_circuit.add_straight_pipes_circ(r__m=cool_inlet_pipe_d__m/2,
-													n_pipes=4,
-													len__m=cool_inlet_pipe_len__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s/4,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-													label="Inlet pipe")
+	pressure_circuit.add_straight_pipes_circ(	r__m=cool_inlet_pipe_d__m/2,
+												n_pipes=4,
+												len__m=cool_inlet_pipe_len__m,
+												label="Inlet pipe"	)
 
 	# inlet distribution ring
-	pressure_circuit.add_circular_pipe_rectangular_box(r__m=rs__m[0]+inputParameters.wallThickness__m,
-															w__m=cool_inlet_ring_w__m,
-															h__m=cool_inlet_ring_h__m,
-															percentage_travelled=0.125,
-															mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s/4,
-															rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-															mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-															n=4,
-															label="Inlet distribution")
+	pressure_circuit.add_circular_pipe_rectangular_box(	r__m=coolingParameters.rs__m[0]+coolingParameters.wallThickness__m,
+														w__m=cool_inlet_ring_w__m,
+														h__m=cool_inlet_ring_h__m,
+														percentage_travelled=0.125,
+														n_pipes=4,
+														label="Inlet distribution"	)
 	# turn into the cooling channels
-	pressure_circuit.add_90deg_turn_annulus(r_inner__m=rs__m[0]+inputParameters.wallThickness__m,
-													r_outer__m=cool_inlet_ring_r__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													label="90 deg turn into channels")
+	pressure_circuit.add_90deg_turn_annulus(	r_inner__m=coolingParameters.rs__m[0]+coolingParameters.wallThickness__m,
+												r_outer__m=cool_inlet_ring_r__m,
+												label="90 deg turn into channels"	)
 	# area change into cooling channels
-	A_in__m2 = np.pi*(pow(cool_inlet_ring_r__m,2)-pow(rs__m[0]+inputParameters.wallThickness__m,2))
-	A_out__m2 = inputParameters.channelHeight__m*channel_width__m*inputParameters.channelCount
-	Dh_in__m = (cool_inlet_ring_r__m*2)-((rs__m[0]+inputParameters.wallThickness__m)*2)
-	pressure_circuit.add_square_area_reduction(A_in__m2=A_in__m2,
-													A_out__m2=A_out__m2,
-													Dh_in__m=Dh_in__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-													label="Area change into channels")
+	A_in__m2 = np.pi*(pow(cool_inlet_ring_r__m,2)-pow(coolingParameters.rs__m[0]+coolingParameters.wallThickness__m,2))
+	A_out__m2 = coolingParameters.channelHeight__m*channel_width__m*coolingParameters.channelCount
+	Dh_in__m = (cool_inlet_ring_r__m*2)-((coolingParameters.rs__m[0]+coolingParameters.wallThickness__m)*2)
+
+	pressure_circuit.add_square_area_reduction(	A_in__m2=A_in__m2,
+												A_out__m2=A_out__m2,
+												Dh_in__m=Dh_in__m,
+												label="Area change into channels"	)
 
 	pressure_circuit.add_manual_pressure_drop(abs(cooling_data["p_coolant"][-1]-cooling_data["p_coolant"][0]), label="Channels friction")
 
-
 	# area change out of cooling channels
-	A_in__m2 = inputParameters.channelHeight__m*channel_width__m*inputParameters.channelCount
-	A_out__m2 = np.pi*(pow(cool_outlet_ring_r__m,2)-pow(rs__m[-1]+inputParameters.wallThickness__m,2))
-	Dh_in__m = ((2*inputParameters.channelHeight__m*channel_width__m)/(inputParameters.channelHeight__m+channel_width__m))*inputParameters.channelCount
-	pressure_circuit.add_square_area_expansion(A_in__m2=A_in__m2,
-													A_out__m2=A_out__m2,
-													Dh_in__m=Dh_in__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-													label="Area change out of channels")
+	A_in__m2 = coolingParameters.channelHeight__m*channel_width__m*coolingParameters.channelCount
+	A_out__m2 = np.pi*(pow(cool_outlet_ring_r__m,2)-pow(coolingParameters.rs__m[-1]+coolingParameters.wallThickness__m,2))
+	Dh_in__m = ((2*coolingParameters.channelHeight__m*channel_width__m)/(coolingParameters.channelHeight__m+channel_width__m))*coolingParameters.channelCount
+
+	pressure_circuit.add_square_area_expansion(	A_in__m2=A_in__m2,
+												A_out__m2=A_out__m2,
+												Dh_in__m=Dh_in__m,
+												label="Area change out of channels"	)
 	# turn out of the cooling channels
-	pressure_circuit.add_90deg_turn_annulus(r_inner__m=rs__m[-1]+inputParameters.wallThickness__m,
-													r_outer__m=cool_outlet_ring_r__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													label="90 deg turn out of channels")
+	pressure_circuit.add_90deg_turn_annulus(	r_inner__m=coolingParameters.rs__m[-1]+coolingParameters.wallThickness__m,
+												r_outer__m=cool_outlet_ring_r__m,
+												label="90 deg turn out of channels"	)
 	# outlet distribution ring
-	pressure_circuit.add_circular_pipe_rectangular_box(r__m=rs__m[-1]+inputParameters.wallThickness__m,
-															w__m=cool_outlet_ring_w__m,
-															h__m=cool_outlet_ring_h__m,
-															percentage_travelled=0.125,
-															mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s/4,
-															rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-															mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-															n=4,
-															label="Oulet distribution")
+	pressure_circuit.add_circular_pipe_rectangular_box(	r__m=coolingParameters.rs__m[-1]+coolingParameters.wallThickness__m,
+														w__m=cool_outlet_ring_w__m,
+														h__m=cool_outlet_ring_h__m,
+														percentage_travelled=0.125,
+														n_pipes=4,
+														label="Oulet distribution"	)
 
 	# outlet pipe
-	pressure_circuit.add_straight_pipes_circ(r__m=cool_outlet_pipe_d__m/2,
-													n_pipes=cool_outlet_pipe_n,
-													len__m=cool_outlet_pipe_len__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s/4,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-													label="Outlet pipe")
+	pressure_circuit.add_straight_pipes_circ(	r__m=cool_outlet_pipe_d__m/2,
+												n_pipes=cool_outlet_pipe_n,
+												len__m=cool_outlet_pipe_len__m,
+												label="Outlet pipe"	)
 
 	# cooling jacket outlet turn into halo
-	pressure_circuit.add_90deg_turn_circular(r__m=cool_outlet_pipe_d__m/2,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s/4,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													n=3,
-													label="Jacket outlet into halo")
+	pressure_circuit.add_90deg_turn_circular(	r__m=cool_outlet_pipe_d__m/2,
+												n_pipes=3,
+												label="Jacket outlet into halo"	)
 													
 	# halo distribution ring
-	pressure_circuit.add_circular_pipe_circular(r__m=cool_halo_ring_d__m/2,
-														rp__m=cool_halo_tube_d__m/2,
-														percentage_travelled=0.5,
-														mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-														rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-														mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-														n=1,
-														label="Halo distribution outlet")
+	pressure_circuit.add_circular_pipe_circular(	r__m=cool_halo_ring_d__m/2,
+													rp__m=cool_halo_tube_d__m/2,
+													percentage_travelled=0.5,
+													n_pipes=1,
+													label="Halo distribution outlet"	)
 														
 	# halo outlet pipe 
-	pressure_circuit.add_straight_pipes_circ(r__m=cool_halo_pipe_d__m/2,
-													n_pipes=1,
-													len__m=cool_halo_outlet_len__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-													label="Halo outlet pipe")
+	pressure_circuit.add_straight_pipes_circ(	r__m=cool_halo_pipe_d__m/2,
+												n_pipes=1,
+												len__m=cool_halo_outlet_len__m,
+												label="Halo outlet pipe"	)
 
 	# return pipe
-	pressure_circuit.add_straight_pipes_circ(r__m=cool_return_d__m/2,
-													n_pipes=1,
-													len__m=cool_return_len__m,
-													mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-													rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-													mu__Pas=inputParameters.coolant.viscosity__Pa_s(T, p),
-													label="Return pipe")
+	pressure_circuit.add_straight_pipes_circ(	r__m=cool_return_d__m/2,
+												n_pipes=1,
+												len__m=cool_return_len__m,
+												label="Return pipe"	)
 	# into bucket
-	pressure_circuit.add_orifice(r__m=cool_return_d__m/2,
-										n_ori=1,
-										CD=cool_CD,
-										mdot__kg_s=inputParameters.coolantMassFlowRate__kg_per_s,
-										rho__kg_m3=inputParameters.coolant.density__kg_per_m3(T, p),
-										label="Into bucket")
-
+	pressure_circuit.add_orifice(	r__m=cool_return_d__m/2,
+									n_ori=1,
+									CD=cool_CD,
+									label="Into bucket"	)
+	
 	# complete pressure drop calcs
-	pressure_circuit.calc_total_pressure_drop()
+	totalPressureDrop__bar = pressure_circuit.calc_total_pressure_drop() / 100000
+
+	return totalPressureDrop__bar
+
+def calculateInjectorManifoldPressureDrop(fuelDensity__kg_per_m3, fuelViscosity__Pa_s, oxidiserDensity__kg_per_m3, oxidiserViscosity__Pa_s):
+	fuelCircuit = PressureDropCircuit(fuelDensity__kg_per_m3, fuelViscosity__Pa_s)
+	oxidiserCircuit = PressureDropCircuit(oxidiserDensity__kg_per_m3, oxidiserViscosity__Pa_s)
+	
+	# TODO: Write all the stuff here...
+	
+	# complete pressure drop calcs
+	fuelPressureDrop__bar = fuelCircuit.calc_total_pressure_drop() / 100000
+	oxidiserPressureDrop__bar = oxidiserCircuit.calc_total_pressure_drop() / 100000
+
+	return fuelPressureDrop__bar, oxidiserPressureDrop__bar
 
 def main():
 	# Generate the finetuned engine parameters
 	rafiki = Rafiki(InputParameters, displayBranding=False)
 	rafikiOutput = rafiki.performConicalNozzleSizing()
-	
 	displayFineTunedParameters(rafikiOutput)
 
 	# Calculate the burn time
@@ -293,16 +263,20 @@ def main():
 								Injector	)
 
 	# Perform the pressure drop calculations
-	calculateCoolantPressureDrop(FineTunedRafikiOutputParameters, CoolingParameters, yetiOutput)
+	coolantPressureDrop__bar = calculateCoolantPressureDrop(CoolingParameters, yetiOutput)
+	'''fuelPressureDrop__bar, oxidiserPressureDrop__bar = calculateInjectorManifoldPressureDrop(	FuelDensity__kg_per_m3,
+																								FuelViscosity__Pa_s,
+																								OxidiserDensity__kg_per_m3,
+																								OxidiserViscosity__Pa_s	)'''
 
-	# TODO: Write this function...
-	#calculateInjectorPressureDrop()
+	#fuelPressureDrop__bar += Injector.injectorPressureDrop__bar
+	#oxidiserPressureDrop__bar += Injector.injectorPressureDrop__bar
 
 	# Display output
 	print("\n*** RESULTS ***")
 	print("PERFORMANCE:")
 	print(f"\tSPECIFIC IMPULSE: %.2f s" % FineTunedRafikiOutputParameters.Isp__s.value)
-	print(f"\tBURN TIME: %.0f s" % maximumBurnTime__s)
+	print(f"\tBURN TIME: %.1f s" % maximumBurnTime__s)
 
 	print("\nDIMENSIONS:")
 	print(f"\tCHAMBER COORDINATES: %s MM" % str(FineTunedRafikiOutputParameters.engineGeometry__mm.value))
@@ -311,7 +285,10 @@ def main():
 
 	print("\nREQUIREMENTS:")
 	print(f"\tFUEL MASS FLOW RATE: %.2f kg/s" % FineTunedRafikiOutputParameters.mf_dot__kg_per_s.value)
-	print(f"\tOXIDISER MASS FLOW RATE: %.2f kg/s" % FineTunedRafikiOutputParameters.mf_dot__kg_per_s.value)
+	print(f"\tOXIDISER MASS FLOW RATE: %.2f kg/s" % FineTunedRafikiOutputParameters.mo_dot__kg_per_s.value)
+	print(f"\tCOOLANT PRESSURE DROP: %.2f bar" % coolantPressureDrop__bar)
+	#print(f"\tFUEL PRESSURE DROP: %.2f bar" % fuelPressureDrop__bar)
+	#print(f"\tOXIDISER PRESSURE DROP: %.2f bar" % oxidiserPressureDrop__bar)
 	print("***************")
 
 if __name__ == "__main__":
